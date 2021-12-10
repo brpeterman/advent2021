f69eb3030f8f65963dbd33f43b8fb45a2410eda6
Brandon Peterman <brpeterm@amazon.com>
2021-12-10 12:31:04 -0600

    Fix broken part 1 scoring, DRY refactor

--- src/main/kotlin/com/github/brpeterman/advent2021/Day10.kt
+++ src/main/kotlin/com/github/brpeterman/advent2021/Day10.kt
@@ -8,18 +8,7 @@ class Day10 {
     }
 
     fun scoreLineError(line: List<String>): Long {
-        val stack = LinkedList<String>()
-        for (char in line) {
-            if (CHUNK_DELIMITERS.keys.contains(char)) {
-                stack.push(char)
-            } else {
-                val chunkStart = stack.pop()
-                if (char != CHUNK_DELIMITERS[chunkStart]) {
-                    return SCORES[char]!!
-                }
-            }
-        }
-        return 0
+        return scoreLine(line, { char -> ERROR_SCORES[char]!! }, { 0 })
     }
 
     fun scoreCompletions(lines: List<List<String>>): Long {
@@ -30,6 +19,10 @@ class Day10 {
     }
 
     fun scoreLineCompletion(line: List<String>): Long {
+        return scoreLine(line, { 0 }, { stack -> stack.fold(0L, { totalScore, char -> totalScore * 5 + COMPLETION_SCORES[CHUNK_DELIMITERS[char]!!]!! })})
+    }
+
+    fun scoreLine(line: List<String>, errorScorer: (String) -> Long, completionScorer: (List<String>) -> Long): Long {
         val stack = LinkedList<String>()
         for (char in line) {
             if (CHUNK_DELIMITERS.keys.contains(char)) {
@@ -37,11 +30,11 @@ class Day10 {
             } else {
                 val chunkStart = stack.pop()
                 if (char != CHUNK_DELIMITERS[chunkStart]) {
-                    return 0 // Corrupt line, doesn't count
+                    return errorScorer(char)
                 }
             }
         }
-        return stack.fold(0L, { totalScore, char -> totalScore * 5 + SCORES[CHUNK_DELIMITERS[char]!!]!! })
+        return completionScorer(stack)
     }
 
     companion object {
@@ -51,11 +44,13 @@ class Day10 {
             Pair("{", "}"),
             Pair("<", ">")
         )
-        val SCORES = mapOf(
+        val ERROR_SCORES = mapOf(
             Pair(")", 3L),
             Pair("]", 57L),
             Pair("}", 1197L),
-            Pair(">", 25137L),
+            Pair(">", 25137L)
+        )
+        val COMPLETION_SCORES = mapOf(
             Pair(")", 1L),
             Pair("]", 2L),
             Pair("}", 3L),
